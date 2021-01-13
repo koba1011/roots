4492b41d9724b97977d1016c0ac2c00c99529342
kobahumi <mattyaminto@gmail.com>
2021-01-13 19:40:14 +0900

    usersモデルテスト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :email, uniqueness: true
   with_options presence: true do
     validates :name
     validates :user_name, format: { with: /\A[a-zA-Z0-9]+\z/, message: "Please type using half-width characters"}
