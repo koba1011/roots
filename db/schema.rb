50916b2d7f4d33f1c65f0c31f4c9e81ea713d5e5
kobahumi <mattyaminto@gmail.com>
2021-01-14 00:25:56 +0900

    マイグレート実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_10_154832) do
+ActiveRecord::Schema.define(version: 2021_01_13_152136) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "caption", null: false
+    t.text "describe"
+    t.string "place"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -27,4 +37,5 @@ ActiveRecord::Schema.define(version: 2021_01_10_154832) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
