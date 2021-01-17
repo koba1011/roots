a13487d747cb0a76917e0ec62cb52a48d5a42ea3
kobahumi <mattyaminto@gmail.com>
2021-01-18 00:52:46 +0900

    edit,updateを定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :set_post, only: [:show, :edit, :update]
 
   def index
     @posts = Post.all.order("created_at DESC")
@@ -27,12 +28,29 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def edit
+    if @post.user_id != current_user.id
+      redirect_to root_path
+    end 
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to :show
+    else
+      render :edit
+    end
   end
 
   private
 
   def post_params
     params.require(:post).permit(:video, :caption, :describe, :place, :share_url).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
