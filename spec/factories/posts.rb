989f0661572d432b47e4f86f2b1b9be24987be01
kobahumi <mattyaminto@gmail.com>
2021-02-02 20:46:20 +0900

    post　単体テスト

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :post do
-    
+    caption     {Faker::Lorem.sentence}
+    describe    {""}
+    place       {""}
+    share_url   {""}
+    youtube_url {""}
+
+    association :user
+
+    after(:build) do |post|
+      post.video.attach(io: File.open('public/videos/test.mp4'), filename: 'test.mp4')
+    end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @post = FactoryBot.build(:post)
+    end
+    
+    context '投稿ができるとき' do
+      it 'videoとcaptionが存在していれば保存できること' do
+        expect(@post).to be_valid
+      end
+
+      it 'youtube_urlとcaptionが存在していれば保存できること' do
+        @post.video = nil
+        @post.youtube_url = "https://www.youtube.com/watch?v=7NVEpjR9xak"
+        expect(@post).to be_valid
+      end
+    end
+
+    context '投稿ができないとき' do
+      it 'videoとyoutube_urlが両方選択されていると保存できないこと' do
+        @post.youtube_url = "https://www.youtube.com/watch?v=7NVEpjR9xak"
+        @post.valid?
+        expect(@post.errors.full_messages).to include("ビデオまたはYoutube動画URLのどちらか一方を選択してください")
+      end
+
+      it 'videoかyoutube_urlどちらも選択されていないと保存できないこと' do
+        @post.video = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("ビデオまたはYoutube動画URLのどちらか一方を選択してください")
+      end
+
+      it 'captionが空では保存できないこと' do
+        @post.caption = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("タイトルを入力してください")
+      end
+    end
+  end
 end
