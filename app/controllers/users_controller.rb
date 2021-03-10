ac30a6e5478b04c1395a5d53e7545a45c03acc54
kobahumi <mattyaminto@gmail.com>
2021-03-10 15:53:02 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, except: [:show]
+  before_action :authenticate_user!, except: [:show, :following, :followers]
   before_action :set_user, only: [:show, :edit, :update, :following, :followers]
   
   def show
